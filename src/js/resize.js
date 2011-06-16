1dad7dbb76ce80be3ab3d51712d91525dbbe88c7
Jeff Tougas <tougasj@gmail.com>
2011-06-16 10:58:02 -0400

    more resize stuff

--- src/js/resize.js
+++ src/js/resize.js
@@ -1,4 +1,6 @@
 function resize(height) {
 	//alert("height: " + height);
 	$('#flexStuff').height(height);
+	
+	//Callback into flex that passes $('body').height() as parameter to be used by queue
 }
